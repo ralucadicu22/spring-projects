e7963921b16ecf16a729b343af1219c4b6bf2278
Raluca <dicuraluca22@yahoo.com>
2024-10-31 17:51:56 +0200

    Added registration form, registed users to the database

--- src/main/java/com/example/login/AppController.java
+++ src/main/java/com/example/login/AppController.java
@@ -1,8 +1,11 @@
 package com.example.login;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AppController {
@@ -11,5 +14,17 @@ public class AppController {
     @GetMapping("/")
     public String viewHomePage(){
         return "index";
+    }
+    @GetMapping("/register")
+    public String viewRegisterPage(Model model){
+        model.addAttribute("user",new Users());
+        return "registration_form";
+    }
+    @PostMapping("/process_register")
+    public String successfulRegistration(Users user){
+        BCryptPasswordEncoder passwordEncode=new BCryptPasswordEncoder();
+        String encodedPassword=passwordEncode.encode(user.getPassword());
+        repository.save(user);
+        return "successful";
     }
 }
